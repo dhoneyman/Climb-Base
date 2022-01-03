05c6c094f4a711397ba1e485a90a7397617a3aa7
Charlie Stedman <9369467+cbs9646@users.noreply.github.com>
2022-01-03 13:19:34 -0600

    updated routes.js

--- models/routes.js
+++ models/routes.js
@@ -1,16 +1,40 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Location extends Model {
+class Route extends Model {
 }
 
-User.init(
-    {
+Route.init(
+    { name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
       id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true,
       },
-
-module.exports = 
+      difficulty: {
+          type: DataTypes.CHAR,
+          allowNull: false,
+      },
+      rating: {
+          type: DataTypes.INT,
+      },
+      location_id: {
+          type: DataTypes.INT,
+          allowNull: false,
+      },
+      wall_id: {
+          type: DataTypes.INT,
+      },
+      user_id: {
+          type: DataTypes.CHAR,
+      },
+      FA: {
+          type: DataTypes.CHAR,
+      },
+    }
+);    
+module.exports = Route;
